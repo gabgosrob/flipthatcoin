9e823af6da72fd7905333d6b85113cf7f8c7fe87
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-05-08 17:57:58 -0400

    add loading and auth to play page

--- pages/play.js
+++ pages/play.js
@@ -1,6 +1,31 @@
+import Router from "next/router";
+import { useState, useEffect } from "react";
+
+import { verify } from "../utils/accounts.js";
 import Header from "../components/header";
+import Loading from "../components/loading";
 
 export default function Play() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        verify().then((user) => {
+            if (!user) {
+                Router.push("/login");
+            } else {
+                setUser(user);
+            }
+        });
+    }, []);
+
+    if (!user) {
+        return (
+            <div>
+                <Loading />
+            </div>
+        );
+    }
+
     return (
         <div>
             <Header loggedIn={true} />
