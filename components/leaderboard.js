6c5976517f47a167ba4b050bb497ca03630e49b3
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-05-10 01:34:06 -0400

    add styling to leaderboard table

--- components/leaderboard.js
+++ components/leaderboard.js
@@ -2,19 +2,21 @@ import styles from "../styles/components/Leaderboard.module.css";
 
 export default function Leaderboard(props) {
     return (
-        <div>
+        <div className={styles.leaderboardContainer}>
             <h2 className={styles.title}>Leaderboard</h2>
-            <table>
+            <table className={styles.leaderboard}>
                 <thead>
-                    <tr>
+                    <tr className={styles.row}>
+                        <th></th>
                         <th>Name</th>
                         <th>Rating</th>
                         <th>Wins</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {props.leaders.map((user) => (
-                        <tr key={user.username}>
+                <tbody className={styles.body}>
+                    {props.leaders.map((user, position) => (
+                        <tr key={user.username} className={styles.row}>
+                            <td>{position + 1}</td>
                             <td>{user.username}</td>
                             <td>{user.rating}</td>
                             <td>{user.wins}</td>
