b84c1b3c8f7d7ff45ff726f5fce9e40c4a20dd03
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-05-11 02:35:25 -0400

    add styling to play page

--- components/game.js
+++ components/game.js
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Cookies from "js-cookie";
+
+import styles from "../styles/components/Game.module.css";
 
 export default function Game(props) {
     const [lastGame, setLastGame] = useState(null);
@@ -26,19 +28,34 @@ export default function Game(props) {
         return <button onClick={play}>Click here to play</button>;
     } else {
         return (
-            <div>
-                <div>
-                    <div>{lastGame.username}</div>
-                    <div>{lastGame.newRating}</div>
-                    <div>{lastGame.ratingDelta}</div>
+            <div className={styles.mainContainer}>
+                <div className={styles.gameContainer}>
+                    <div className={styles.playerContainer}>
+                        <h2 className={styles.title}>YOURSELF</h2>
+                        <h3 className={styles.subtitle}>Username</h3>
+                        <div>{lastGame.username}</div>
+                        <h3 className={styles.subtitle}>New Rating</h3>
+                        <div>{lastGame.newRating}</div>
+                        <h3 className={styles.subtitle}>Change</h3>
+                        <div>
+                            {lastGame.ratingDelta > 0 ? "+" : ""}
+                            {lastGame.ratingDelta}
+                        </div>
+                    </div>
+                    <div>{lastGame.result ? "WIN!!!" : "LOSS!!!"}</div>
+                    <div className={styles.playerContainer}>
+                        <h2 className={styles.title}>OPPONENT</h2>
+                        <h3 className={styles.subtitle}>Username</h3>
+                        <div>{lastGame.opponentUsername}</div>
+                        <h3 className={styles.subtitle}>New Rating</h3>
+                        <div>{lastGame.opponentNewRating}</div>
+                        <h3 className={styles.subtitle}>Change</h3>
+                        <div>
+                            {lastGame.opponentRatingDelta > 0 ? "+" : ""}
+                            {lastGame.opponentRatingDelta}
+                        </div>
+                    </div>
                 </div>
-                <div>{lastGame.result ? "WIN!!!" : "LOSS!!!"}</div>
-                <div>
-                    <div>{lastGame.opponentUsername}</div>
-                    <div>{lastGame.opponentNewRating}</div>
-                    <div>{lastGame.opponentRatingDelta}</div>
-                </div>
-
                 <button onClick={play}>Click here to play</button>
             </div>
         );
